01601abd776f822730e2850acf0e5eddf698b5c5
Wyen GW <spiral@Spiralrae.local>
2025-01-21 20:26:44 -0800

    added cypress and basic e2e test on initial navigation

---
+++ app/(client)/about/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function AboutPage() {
+  return (
+    <section>
+      <div className="container">
+        <h1>About</h1>
+      </div>
+    </section>
+  );
+}
