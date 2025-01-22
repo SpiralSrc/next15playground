fe138e29b5dcb98d6a754c786d2f122fa4d4baa1
Wyen GW <spiral@Spiralrae.local>
2025-01-21 19:30:07 -0800

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section className="w-full h-full">
+      <div className="container">
+        <h1>Hello World!</h1>
+      </div>
+    </section>
+  );
+}
